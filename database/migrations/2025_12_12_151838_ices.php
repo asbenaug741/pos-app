c275d367c61ca7d085c8f5bcd3b1e4bc9b565072
Rafli Nailur Rizqi <raflinailurrizqi@gmail.com>
2025-12-13 01:51:48 +0700

    repair forgotten

--- database/migrations/2025_12_12_151838_ices.php
+++ database/migrations/2025_12_12_151838_ices.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sugar', function (Blueprint $table) {
+        Schema::create('ices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sugar_code');
+            $table->string('ices_code');
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sugar');
+        Schema::dropIfExists('ices');
     }
 };
--- database/migrations/2025_12_12_151906_sugars.php
+++ database/migrations/2025_12_12_151906_sugars.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ices', function (Blueprint $table) {
+        Schema::create('sugars', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('ice_code');
+            $table->string('sugars_code');
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ices');
+        Schema::dropIfExists('sugars');
     }
 };
